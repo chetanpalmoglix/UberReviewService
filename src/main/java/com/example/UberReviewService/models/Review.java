984db9f7e4c92946ded86bb5a4fcf206aeace365
chetanpalmoglix <chetan.pal@moglix.com>
2025-07-01 18:39:45 +0530

    added more field

--- src/main/java/com/example/UberReviewService/models/Review.java
+++ src/main/java/com/example/UberReviewService/models/Review.java
@@ -1,10 +1,34 @@
 package com.example.UberReviewService.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
 
 @Entity
+@Table(name = "bookingreview")
 public class Review {
     @Id
-    long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Identity means auto_increment
+    Long id;
+
+    @Column(nullable = false)
+    String content;
+
+    Double rating;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP) // this annotation tells spring about the formats of Date object to be stored i.e. Date / Time ? Timestamp
+    @CreatedDate // this annotation tells spring that only handle it for object creation
+    Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate  // this annotation tells spring that only handle it for object update
+    Date updatedAt;
 }
