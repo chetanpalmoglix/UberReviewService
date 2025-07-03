dd687f1dcf446ad685b3274e99f657f9ea5983cc
chetanpalmoglix <chetan.pal@moglix.com>
2025-07-03 15:14:05 +0530

    Added basemodel class using mappedsuperclass

--- src/main/java/com/example/UberReviewService/models/Review.java
+++ src/main/java/com/example/UberReviewService/models/Review.java
@@ -6,45 +6,23 @@ package com.example.UberReviewService.models;
 //import jakarta.persistence.Id;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.util.Date;
 
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Table(name = "bookingreview")
-public class Review {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // Identity means auto_increment
-    private Long id;
+public class Review extends BaseModel{
 
     @Column(nullable = false)
     private String content;
 
     private Double rating;
 
-    @Column(nullable = false)
-    @Temporal(TemporalType.TIMESTAMP) // this annotation tells spring about the formats of Date object to be stored i.e. Date / Time ? Timestamp
-    @CreatedDate // this annotation tells spring that only handle it for object creation
-    private Date createdAt;
-
-    @Column(nullable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @LastModifiedDate  // this annotation tells spring that only handle it for object update
-    private Date updatedAt;
-
-
     @Override
     public String toString() {
         return "Review: " + this.content + " " + this.rating + " " + this.createdAt;
     }
-
-
 }
