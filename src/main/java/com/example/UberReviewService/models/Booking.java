5d4b1f220998a851d49a3497385f5a663313cb09
chetanpalmoglix <chetan.pal@moglix.com>
2025-07-04 14:29:45 +0530

    Added booking associated to review

---
+++ src/main/java/com/example/UberReviewService/models/Booking.java
@@ -0,0 +1,30 @@
+package com.example.UberReviewService.models;
+
+import jakarta.persistence.*;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Booking  extends BaseModel{
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private Review review; // we have defined a 1:1 relationship between booking and review
+
+    @Enumerated(value = EnumType.STRING)
+    private BookingStatus bookingStatus;
+
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date startTime;
+
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date endTime;
+
+    private Long totalDistance;
+}
--- src/main/java/com/example/UberReviewService/services/ReviewService.java
+++ src/main/java/com/example/UberReviewService/services/ReviewService.java
@@ -1,34 +1,48 @@
 package com.example.UberReviewService.services;
 
+import com.example.UberReviewService.models.Booking;
 import com.example.UberReviewService.models.Review;
+import com.example.UberReviewService.repositories.BookingRepository;
 import com.example.UberReviewService.repositories.ReviewRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReviewService implements CommandLineRunner {
     private ReviewRepository reviewRepository;
+    private final BookingRepository bookingRepository;
 
-    public ReviewService(ReviewRepository reviewRepository){
+    public ReviewService(ReviewRepository reviewRepository, BookingRepository bookingRepository){
         this.reviewRepository=reviewRepository;
+        this.bookingRepository = bookingRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("*******");
-        Review r=Review.builder().
-                content("Amazon ride Quality").
-                rating(5.6).build(); // code to create plain java object
-        System.out.println(r);
-        reviewRepository.save(r);   // this code executes sql query
+        Review r = Review.builder()
+        .content("Amazing ride quality")
+        .rating(4.0).build();
 
+        Booking b = Booking
+                    .builder()
+                    .review(r)
+                    .endTime(new Date())
+                    .build();
+        bookingRepository.save(b);     // this code executes sql query
+        System.out.println(r);
+        System.out.println(r.getId());
         List<Review> reviews = reviewRepository.findAll();
         for(Review review : reviews) {
             System.out.println(r.getContent());
         }
+//        Optional<Booking> b = bookingRepository.findById(6L);
+//        if(b.isPresent()) {
+//            bookingRepository.delete(b.get());
+//        }
 //        reviewRepository.deleteById(2L);
     }
 }
