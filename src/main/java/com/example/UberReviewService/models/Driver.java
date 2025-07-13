96e81502f91c04cb6148006c6447b19a5a839deb
chetanpalmoglix <chetan.pal@moglix.com>
2025-07-13 12:56:18 +0530

    Added one to many relation between booking and driver, booking and passenger

---
+++ src/main/java/com/example/UberReviewService/models/Driver.java
@@ -0,0 +1,27 @@
+package com.example.UberReviewService.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Driver extends BaseModel{
+    private String name;
+
+    @Column(nullable = false,unique = true)
+    private String licenseNumber;
+
+    // 1 : n , Driver : Booking
+    @OneToMany(mappedBy = "driver")
+    private List<Booking> bookings=new ArrayList<>();
+
+}
---
+++ src/main/java/com/example/UberReviewService/models/Passenger.java
@@ -0,0 +1,22 @@
+package com.example.UberReviewService.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Passenger extends BaseModel{
+    private String name;
+
+    @OneToMany(mappedBy = "passenger")
+    private List<Booking> bookings=new ArrayList<>();
+}
